775ba8fa5de6536214b788e4e0336822cc530bd2
Abel Marin <marinabe1416@gmail.com>
2023-12-15 22:38:18 -0500

    Add 500 page

---
+++ pages/500.js
@@ -0,0 +1,25 @@
+import { getContactDetails, getSocialMedia } from "storyblok/api"
+
+export default function Page() {
+  return (
+    <main>
+      <section className="px-6 py-8 max-w-screen-lg mx-auto shadow-section rounded-3xl mt-12">
+        <div>
+          <h1>500 Internal Server Error</h1>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export async function getStaticProps() {
+  const socialMedia = await getSocialMedia()
+  const contactDetails = await getContactDetails()
+
+  return {
+    props: {
+      socialMedia,
+      contactDetails,
+    },
+  }
+}

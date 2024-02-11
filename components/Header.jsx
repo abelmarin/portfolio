c27ef1f286a141737ba243679b1c7fd38b9d89b5
Abel Marin <marinabe1416@gmail.com>
2024-02-10 22:03:19 -0500

    Add hamburger menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,26 @@
 import Link from "next/link"
 import GitHub from "public/assets/github.svg"
 import LinkedIn from "public/assets/linkedin.svg"
+import Hamburger from "public/assets/hamburger-menu-button.svg"
 
-export default function Header({ socialMedia }) {
+export default function Header({ socialMedia, jobs }) {
   return (
     <header className="mx-auto flex flex-col sm:flex-row gap-4 justify-between items-center pb-4 mb-12 border-b border-white">
       <div>
-        <Link href="/">
-          <h1 className="font-light text-l2 lg:text-xl2">Abel Marin Jr.</h1>
+        <Link href="/" className="font-light text-l2 lg:text-xl2">
+          Abel Marin Jr.
         </Link>
-        <h2 className="text-m2 text-center sm:text-left">Web Developer</h2>
+        <div className="text-m2 text-center sm:text-left">Web Developer</div>
       </div>
       <nav className="flex gap-4 items-center">
-        <Link href="/projects" className="hover:underline">
-          Projects
-        </Link>
+        <div className="group">
+          <Link href="/projects" className="hover:underline">
+            Projects
+          </Link>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
         <Link href="/contact" className="hover:underline">
           Contact
         </Link>

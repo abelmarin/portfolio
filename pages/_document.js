c8d4e551e3532a6128ca6fb2cd309f5eaeca1fc0
Abel Marin <marinabe1416@gmail.com>
2024-03-02 23:16:57 -0500

    Add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-YNG63QF8B2" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YNG63QF8B2');
+          `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />

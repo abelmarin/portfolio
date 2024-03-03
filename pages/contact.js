93e2e18e81f8349a3721c69f383bd925bf901af6
Abel Marin <marinabe1416@gmail.com>
2024-03-03 12:57:59 -0500

    Add resume link

--- pages/contact.js
+++ pages/contact.js
@@ -6,17 +6,19 @@ import { getGlobals } from "storyblok/api"
 export default function Contact({ socialMedia, contactDetails }) {
   return (
     <section>
+      <a
+        href="/Abel-Marin-Resume.docx"
+        download
+        className="mb-6 px-4 pt-2 pb-3 text-m3 lg:text-l2 border-2 border-white w-max mx-auto lg:mx-0 !no-underline inline-block"
+      >
+        Download My Resume
+      </a>
+
       <div>
-        Email:{" "}
-        <Link href={`mailto:${contactDetails?.Email}`} className="hover:underline">
-          {contactDetails?.Email}
-        </Link>
+        Email: <Link href={`mailto:${contactDetails?.Email}`}>{contactDetails?.Email}</Link>
       </div>
       <div>
-        Phone:{" "}
-        <Link href={`tel:${contactDetails?.Phone}`} className="hover:underline">
-          {contactDetails?.Phone}
-        </Link>
+        Phone: <Link href={`tel:${contactDetails?.Phone}`}>{contactDetails?.Phone}</Link>
       </div>
 
       <div className="flex gap-4 pt-8">
